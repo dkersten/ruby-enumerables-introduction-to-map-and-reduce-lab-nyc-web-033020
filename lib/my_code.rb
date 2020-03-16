507f985e1ca9fff0a920cc546b530c30097ac6e9
Daniel Kersten <dkersten1994@gmail.com>
2020-03-16 15:52:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,12 @@
 # My Code here....
-map_to_negativize(source_array)
-
+def map_to_negativize(source_array)
+  new_array = []
+  i = 0
+  
+  while i < source_array.length do
+    new_array << (source_array[i] * (-1))
+    
+    i += 1
+  end
+  new_array
 end
