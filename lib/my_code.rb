ed6aae3bde931d0275ced19d540e4587ec12aa2e
Daniel Kersten <dkersten1994@gmail.com>
2020-03-16 15:55:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,7 +40,7 @@ def map_to_square(source_array)
   i = 0
   
   while i < source_array.length do
-    new_array << (source_array[i] * source_array[i]
+    new_array << (source_array[i] * source_array[i])
     
     i += 1
   end
