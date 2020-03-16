fac48e9105d8dc7ba9cbf73e5ac09c2078156982
Daniel Kersten <dkersten1994@gmail.com>
2020-03-16 16:05:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,7 +54,7 @@ def reduce_to_total(source_array, starting_point=0)
   while i < source_array.length do
   total += source_array[i]
   
-  i += 0
+  i += 1
   end
   # source_array.length.times{|i|total = source_array[i] + total}
   total
