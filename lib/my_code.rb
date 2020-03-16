c8313fda448a9548fa7d5499507b7d7c69d3eb5a
Daniel Kersten <dkersten1994@gmail.com>
2020-03-16 16:07:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,3 +58,21 @@ def reduce_to_total(source_array, starting_point=0)
   end
   total
 end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  
+  while i < source_array.length do
+    if !source_array[i]
+      return false
+    end
+    
+    i += 1
+  end
+  true
+end
+
+
+
+
+
